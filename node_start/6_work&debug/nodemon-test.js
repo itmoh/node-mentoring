1e6c8cbb5981f0682a1a28f0bee297b12ce0be41
Maksym_Tarasenko@epam.com <Maksym_Tarasenko@epam.com>
2016-03-02 22:03:04 +0200

    removing unnecessary files

--- node_start/6_work&debug/nodemon-test.js
+++ node_start/6_work&debug/nodemon-test.js
@@ -5,7 +5,7 @@ http.createServer(function(request, response) {
         response.writeHead(200, {
             'Content-Type':'text/html'
         });
-        response.end(JSON.stringify('test'));
+        response.end('testssdas');
 }).listen(3000);
 
 console.log('node server running on port 3000');
